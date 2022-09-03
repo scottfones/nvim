6d1c5cc03d65f7a30947d23418d1967d6a94d7a7
Scott Fones <scott.fones@gmail.com>
2022-09-03 13:45:00 -0400

    fixed colortheme color tweaks

--- lua/user/colorscheme.lua
+++ lua/user/colorscheme.lua
@@ -1,49 +1,67 @@
-function GruvboxColorTweaks()
-    local hl = function(thing, opts)
-        vim.api.nvim_set_hl(0, thing, opts)
-    end
+function GruvboxBabyColorTweaks()
+	local hl = function(thing, opts)
+		vim.api.nvim_set_hl(0, thing, opts)
+	end
 
-    hl("SignColumn", {
-        bg = "none",
-    })
+	hl("SignColumn", {
+		bg = "none",
+	})
 
-    hl("ColorColumn", {
-        ctermbg = 0,
-        bg = "#555555",
-    })
+	hl("ColorColumn", {
+		ctermbg = 0,
+		bg = "#555555",
+	})
 
-    hl("CursorLineNR", {
-        bg = "None"
-    })
+	hl("CursorLineNR", {
+		bg = "None",
+	})
 
-    hl("Normal", {
-        bg = "none"
-    })
+	hl("Normal", {
+		bg = "none",
+	})
 
-    hl("LineNr", {
-        fg = "#5eacd3"
-    })
+	hl("LineNr", {
+		fg = "#5eacd3",
+	})
 
-    hl("netrwDir", {
-        fg = "#5eacd3"
-    })
+	hl("netrwDir", {
+		fg = "#5eacd3",
+	})
+end
 
+function TokyoColorTweaks()
+	vim.api.nvim_set_hl(0, "LineNr", {
+		bg = "None",
+		fg = "#5eacd3",
+	})
 end
 
 --local colorscheme = "darkplus"
-local colorscheme = "gruvbox"
+local colorscheme = "gruvbox-baby"
+--local colorscheme = "tokyonight"
 
 if colorscheme == "gruvbox" then
-  vim.g.gruvbox_contrast_dark = "hard"
-end
-
-if colorscheme == "gruvbox-baby" then
-  vim.g.gruvbox_baby_telescope_theme = true
-  vim.g.gruvbox_baby_transparent_mode = true
-  GruvboxColorTweaks()
+	vim.g.gruvbox_contrast_dark = "hard"
+elseif colorscheme == "gruvbox-baby" then
+	vim.g.gruvbox_baby_background_color = "dark"
+	vim.g.gruvbox_baby_telescope_theme = true
+	vim.g.gruvbox_baby_transparent_mode = true
+elseif colorscheme == "tokyonight" then
+	vim.g.tokyonight_style = "night"
+	vim.g.tokyonight_transparent_sidebar = true
+	vim.g.tokyonight_transparent = true
 end
 
 local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
 if not status_ok then
-  return
+	return
 end
+
+-- Color tweaks must be after theme setup
+
+local color_tweaks = {
+	["gruvbox-baby"] = GruvboxBabyColorTweaks,
+	["tokyonight"] = TokyoColorTweaks,
+}
+
+color_tweaks[colorscheme]()
