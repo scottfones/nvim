4196130a786f42d04862d0e301d4a98764fa812a
Scott Fones <scott.fones@gmail.com>
2023-02-26 14:04:03 -0500

    added theme specific fixes

--- lua/user/colorscheme.lua
+++ lua/user/colorscheme.lua
@@ -1,14 +1,23 @@
 --local colorscheme = "darkplus"
+--[[ local colorscheme = "gruvbox" ]]
 local colorscheme = "gruvbox-baby"
---local colorscheme = "tokyonight"
+--[[ local colorscheme = "tokyonight" ]]
 
 if colorscheme == "gruvbox" then
 	vim.g.gruvbox_contrast_dark = "hard"
+  vim.g.gruvbox_invert_indent_guides = "true"
 elseif colorscheme == "gruvbox-baby" then
 	vim.g.gruvbox_baby_background_color = "dark"
 	vim.g.gruvbox_baby_telescope_theme = true
 elseif colorscheme == "tokyonight" then
-	vim.g.tokyonight_style = "night"
+	require("tokyonight").setup({
+		style = "storm",
+		styles = {
+			sidebars = "transparent",
+		},
+    dim_inactive = false,
+    transparent = true,
+	})
 end
 
 local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
