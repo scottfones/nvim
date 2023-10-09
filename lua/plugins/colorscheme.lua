4f298c08b7f5abf2445d3da5c558e0c2fde1ebb1
Scott Fones <scott.fones@gmail.com>
2023-10-09 14:52:45 -0400

    switch to LazyVim

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,51 @@
+return {
+  -- gruvbox
+  {
+    "ellisonleao/gruvbox.nvim",
+    opts = {
+      transparent_mode = true,
+    },
+  },
+
+  -- gruvbox-baby
+  {
+    "luisiacc/gruvbox-baby",
+    lazy = true,
+    config = function()
+      vim.g.gruvbox_baby_background_color = "dark"
+      vim.g.gruvbox_baby_telescope_theme = 1
+      vim.g.gruvbox_baby_transparent_mode = 1
+    end,
+  },
+
+  -- tokyonight
+  {
+    "folke/tokyonight.nvim",
+    lazy = true,
+    opts = {
+      style = "night",
+      styles = {
+        sidebars = "transparent",
+      },
+      transparent = true,
+    },
+  },
+
+  -- catpuccin
+  {
+    "catppuccin/nvim",
+    lazy = true,
+    name = "catppuccin",
+    opts = {
+      transparent_background = true,
+    },
+  },
+
+  -- Configure LazyVim to load gruvbox
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "gruvbox-baby",
+    },
+  },
+}
