c83d82bcba050fecd2fe65195faead3af4e1d808
Scott Fones <scott.fones@gmail.com>
2024-09-25 14:05:47 -0400

    gruvbox material: enabled opaque background

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -29,7 +29,7 @@ return {
       vim.g.gruvbox_material_enable_bold = "1"
       vim.g.gruvbox_material_enable_italic = "1"
       vim.g.gruvbox_material_foreground = "material"
-      vim.g.gruvbox_material_transparent_background = "2"
+      vim.g.gruvbox_material_transparent_background = "0"
       vim.g.gruvbox_material_ui_contrast = "high"
     end,
   },
